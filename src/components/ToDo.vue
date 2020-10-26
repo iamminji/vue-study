6c9c70415ac62f3d640914a742f5c85dcf026081
soy-latte <soy.latte@kakaoenterprise.com>
2020-10-27 00:54:21 +0900

    버튼 누르는 액션 연습

---
+++ src/components/ToDo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{ text }}</h1>
+        <button v-on:click="btnClick" id="wow">클릭</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ToDo',
+        props: {
+            text: String,
+            message: String
+        },
+        methods: {
+            btnClick: function () {
+                this.text = "버튼 누름!";
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
